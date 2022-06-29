0e78412191a93b6e5d069a090f4374559af4cb38
scolear <bobtera@gmail.com>
2022-06-29 18:14:41 +0200

    ref: cleanup common.ts

--- scripts/common.ts
+++ scripts/common.ts
@@ -9,13 +9,13 @@ export const network = isProd ? new StacksTestnet() : new StacksMocknet();
 
 export const senderAddress = secrets.publicKey;
 export const senderKey = secrets.privateKey;
-export const assetName = 'BTC';
 export const tokenName = 'open-dlc';
-export const strikePrice = 22000;
 
 export const contractAddress = isProd ? "ST12S2DB1PKRM1BJ1G5BQS0AB0QPKHRVHWXDBJ27R" : "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM";
 export const contractName = "discreet-log-storage-v5-1";
 
-export const unixTimeStamp = 1656421534;
-
-export const UUID = "uuid71";
+// Generally only changing these for the scripts:
+export const assetName = 'BTC';
+export const strikePrice = 22222;
+export const unixTimeStamp = 1656516644;
+export const UUID = "uuid84";
